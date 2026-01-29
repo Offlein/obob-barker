a6ec570f1befbb4c7d116f4a65080cffc9e5a50a
Craig Leinoff <craig.leinoff@gmail.com>
2026-01-28 18:04:52 -0800

    WIP

--- src/views/ObobGameplay.vue
+++ src/views/ObobGameplay.vue
@@ -15,63 +15,60 @@ const handleRestartConfirmation = () => {
 </script>
 
 <template>
-  <div
-    class="flex w-full gap-x-8"
-  >
-    <div
-      class="fieldset mt-13 flex flex-col w-1/3"
-    >
+  <div class="flex w-full gap-x-8">
+    <div class="fieldset mt-13 flex flex-col w-1/3">
       <TeamData :team-number="1" />
       <div class="divider divider-horizontal" />
       <TeamData :team-number="2" />
 
       <div class="flex flex-col items-center">
-        <div
-          class="btn btn-neutral mt-2 cursor-pointer w-full"
-          onclick="welcome_modal.showModal()"
-        >
-          Show Welcome Script
+        <div class="flex flex-wrap leading-4 gap-x-4 gap-y-2 justify-center mt-2">
+          <div class="btn btn-neutral cursor-pointer" onclick="welcome_modal.showModal()">
+            Welcome Script
+          </div>
+          <div class="btn btn-neutral cursor-pointer" onclick="scoreboard.show()">
+            Show Scoreboard
+          </div>
         </div>
+
         <div
           class="hover:bg-red-700 hover:text-white mt-2 p-1 rounded-lg w-52 group transition-colors duration-500 cursor-pointer"
           onclick="restart_confirm.showModal()"
         >
           <div class="flex justify-center items-center text-center gap-x-1">
-            <IconClearOctagon class="text-[1rem]"/>
+            <IconClearOctagon class="text-[1rem]" />
             Reset / Start Over
           </div>
-          <div class="transition-opacity duration-500 delay-250 opacity-0 group-hover:opacity-100 text-center">
+          <div
+            class="transition-opacity duration-500 delay-250 opacity-0 group-hover:opacity-100 text-center"
+          >
             Confirmation will be required.
           </div>
         </div>
       </div>
     </div>
 
-    <ObobQuestions
-      class="w-2/3"
-    />
+    <ObobQuestions class="w-2/3" />
 
     <dialog id="restart_confirm" ref="restartModal" class="modal">
       <div class="modal-box text-center">
         <form method="dialog">
           <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
         </form>
-        <h3 class="text-lg font-bold text-error">Are you sure you want to <strong>start over</strong>?</h3>
+        <h3 class="text-lg font-bold text-error">
+          Are you sure you want to <strong>start over</strong>?
+        </h3>
         <p class="py-4">Any progress in this round will be lost.</p>
 
         <div class="flex gap-x-8 justify-center items-center">
-          <div class="cursor-pointer hover:underline" onclick="restart_confirm.close()">Oops! Nevermind.</div>
-          <div
-            class="btn btn-warning"
-            @click="handleRestartConfirmation"
-          >
-            I'm sure!
+          <div class="cursor-pointer hover:underline" onclick="restart_confirm.close()">
+            Oops! Nevermind.
           </div>
+          <div class="btn btn-warning" @click="handleRestartConfirmation">I'm sure!</div>
         </div>
       </div>
     </dialog>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
